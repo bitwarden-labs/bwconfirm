5e6763d06f5b6e33392316c83862e01a63df2c01
Jordan Cooks <notnamed@users.noreply.github.com>
2025-08-21 13:00:01 -0700

    Order of operations

--- bwConfirm.sh
+++ bwConfirm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 mp=$(echo $p0 | base64 -d)
-session_key="$(printf $mp | /bw unlock --raw)"
 if [[ -z "${set_server_url}" ]];
 then
 	/bw config server $bw_server_url
 fi
 /bw login --apikey
+session_key="$(printf $mp | /bw unlock --raw)"
+
 org_members="$(/bw list --session "$session_key" org-members --organizationid $organization_id | jq -c '.[] | select( .status == 1 )' | jq -c '.id' | tr -d '"')"
 for member_id in ${org_members[@]} ;
 	do /bw confirm --session $session_key org-member $member_id --organizationid $organization_id;
